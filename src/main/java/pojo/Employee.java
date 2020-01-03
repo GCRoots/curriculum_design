c67c1d2bf78946cf6b4e09a255b39bca5c442b3d
GC <1007879951@qq.com>
2020-01-03 23:02:11 +0800

    update

--- src/main/java/pojo/Employee.java
+++ src/main/java/pojo/Employee.java
@@ -5,4 +5,14 @@ package pojo;
  * @data 19-12-29
  */
 public class Employee {
+    private String birthday;
+    private String sex;
+    private String staff_id;
+    private String staff_name;
+    private String dep_name;
+    private String parti_time;
+    private String title;
+    private String position;
+    private String political_status;
+    private String marital_status;
 }
