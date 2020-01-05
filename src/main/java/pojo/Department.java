232976362e9262ae685be212c4bcfe8e02928a55
GC <1007879951@qq.com>
2020-01-05 22:48:38 +0800

    update

--- src/main/java/pojo/Department.java
+++ src/main/java/pojo/Department.java
@@ -5,4 +5,50 @@ package pojo;
  * @data 19-12-29
  */
 public class Department {
+    private String dep_id;
+    private String dep_name;
+    private String header;
+    private String number;
+
+    public String getDep_id() {
+        return dep_id;
+    }
+
+    public void setDep_id(String dep_id) {
+        this.dep_id = dep_id;
+    }
+
+    public String getDep_name() {
+        return dep_name;
+    }
+
+    public void setDep_name(String dep_name) {
+        this.dep_name = dep_name;
+    }
+
+    public String getHeader() {
+        return header;
+    }
+
+    public void setHeader(String header) {
+        this.header = header;
+    }
+
+    public String getNumber() {
+        return number;
+    }
+
+    public void setNumber(String number) {
+        this.number = number;
+    }
+
+    @Override
+    public String toString() {
+        return "Department{" +
+                "dep_id='" + dep_id + '\'' +
+                ", dep_name='" + dep_name + '\'' +
+                ", header='" + header + '\'' +
+                ", number='" + number + '\'' +
+                '}';
+    }
 }
--- src/main/java/pojo/Employee.java
+++ src/main/java/pojo/Employee.java
@@ -15,4 +15,100 @@ public class Employee {
     private String position;
     private String political_status;
     private String marital_status;
+
+    public String getBirthday() {
+        return birthday;
+    }
+
+    public void setBirthday(String birthday) {
+        this.birthday = birthday;
+    }
+
+    public String getSex() {
+        return sex;
+    }
+
+    public void setSex(String sex) {
+        this.sex = sex;
+    }
+
+    public String getStaff_id() {
+        return staff_id;
+    }
+
+    public void setStaff_id(String staff_id) {
+        this.staff_id = staff_id;
+    }
+
+    public String getStaff_name() {
+        return staff_name;
+    }
+
+    public void setStaff_name(String staff_name) {
+        this.staff_name = staff_name;
+    }
+
+    public String getDep_name() {
+        return dep_name;
+    }
+
+    public void setDep_name(String dep_name) {
+        this.dep_name = dep_name;
+    }
+
+    public String getParti_time() {
+        return parti_time;
+    }
+
+    public void setParti_time(String parti_time) {
+        this.parti_time = parti_time;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    public String getPosition() {
+        return position;
+    }
+
+    public void setPosition(String position) {
+        this.position = position;
+    }
+
+    public String getPolitical_status() {
+        return political_status;
+    }
+
+    public void setPolitical_status(String political_status) {
+        this.political_status = political_status;
+    }
+
+    public String getMarital_status() {
+        return marital_status;
+    }
+
+    public void setMarital_status(String marital_status) {
+        this.marital_status = marital_status;
+    }
+
+    @Override
+    public String toString() {
+        return "Employee{" +
+                "birthday='" + birthday + '\'' +
+                ", sex='" + sex + '\'' +
+                ", staff_id='" + staff_id + '\'' +
+                ", staff_name='" + staff_name + '\'' +
+                ", dep_name='" + dep_name + '\'' +
+                ", parti_time='" + parti_time + '\'' +
+                ", title='" + title + '\'' +
+                ", position='" + position + '\'' +
+                ", political_status='" + political_status + '\'' +
+                ", marital_status='" + marital_status + '\'' +
+                '}';
+    }
 }
